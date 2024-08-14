2f46460ba1feab5975b6afa019bf9b51216366eb
meius <bohdan.meius@nixs.com>
2024-08-14 11:06:02 +0200

    develop: add error handling to Setting class constructor - Added exception handling for invalid model types in the Setting class constructor. - Ensured that InvalidModelException is thrown when necessary.

--- src/Attributes/Setting.php
+++ src/Attributes/Setting.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Meius\LaravelFilter\Attributes;
 
 use Illuminate\Database\Eloquent\Model;
+use Meius\LaravelFilter\Exceptions\InvalidModelException;
 
 #[\Attribute]
 abstract class Setting
@@ -13,12 +14,21 @@ abstract class Setting
      * Create a new attribute instance.
      *
      * @param  class-string<Model>[]  $models
+     * @throws InvalidModelException
      */
     public function __construct(string ...$models)
     {
+        if (empty($models)) {
+            throw new InvalidModelException("The models array cannot be empty.");
+        }
+
         foreach ($models as $model) {
+            if (!class_exists($model)) {
+                throw new InvalidModelException("The class {$model} does not exist.");
+            }
+
             if (! is_subclass_of($model, Model::class)) {
-                throw new \InvalidArgumentException("The class {$model} must be a subclass of ".Model::class);
+                throw new InvalidModelException("The class {$model} must be a subclass of ".Model::class);
             }
         }
     }
